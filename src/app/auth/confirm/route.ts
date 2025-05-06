a84c8b8a848a877537707138c703056238bc4fb7
JakubLukaszek <jakub.lukaszek@zse.krakow.pl>
2025-05-06 10:30:09 +0200

    refactor: route and middleware improvements

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,15 +1,15 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createClient } from "@/utils/supabase/server";
+  import { NextResponse, type NextRequest } from "next/server";
+  import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const code = searchParams.get("code");
+  export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const code = searchParams.get("code");
 
-  if (code) {
-    const supabase = createClient();
-    (await supabase).auth.exchangeCodeForSession(code);
+    if (code) {
+      const supabase = createClient();
+      (await supabase).auth.exchangeCodeForSession(code);
+    }
+
+    console.log("success");
+    return NextResponse.json("success");
   }
-
-  console.log("success");
-  return NextResponse.json("success");
-}
