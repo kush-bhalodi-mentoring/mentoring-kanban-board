e0a064b8d624a1981812e43c091ce2609ac87458
hadesuu <k.gocek222@gmail.com>
2025-05-09 11:02:54 +0200

    trying to fix [teamId] page

--- src/app/team/[teamId]/page.tsx
+++ src/app/team/[teamId]/page.tsx
@@ -1,17 +1,18 @@
 // src/app/team/[teamId]/page.tsx
 
-import TeamView from "@/views/Team/TeamView"
+import TeamView from "@/views/Team/TeamView";
+import { JSX } from "react";
 
 type PageProps = {
-    params: { teamId: string };
-  };
-  
-  export default async function TeamPage(props: Promise<PageProps>) {
-    const {
-      params: { teamId },
-    } = await props;
-  
-    return <TeamView teamId={teamId} />;
-  }
+  params: { teamId: string };
+};
+
+export default async function TeamPage(props: PageProps): Promise<JSX.Element> {
+  const {
+    params: { teamId },
+  } = props;
+
+  return <TeamView teamId={teamId} />;
+}
 
 
